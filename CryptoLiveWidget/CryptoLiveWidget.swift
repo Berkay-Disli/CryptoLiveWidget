f4a21d459e9665c6f168701f1341af74972bc603
Berkay-Disli <berkaydisli.work@gmail.com>
2022-11-26 18:49:48 +0300

    Widget Build

---
+++ CryptoLiveWidget/CryptoLiveWidget.swift
@@ -0,0 +1,156 @@
+//
+//  CryptoLiveWidget.swift
+//  CryptoLiveWidget
+//
+//  Created by Berkay Disli on 26.11.2022.
+//
+
+import WidgetKit
+import SwiftUI
+import Charts
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> Crypto {
+        Crypto(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (Crypto) -> ()) {
+        let entry = Crypto(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        let currentDate = Date()
+        
+        Task {
+            if var cryptoData = try? await fetchData() {
+                cryptoData.date = currentDate
+                let nextUpdate = Calendar.current.date(byAdding: .minute, value: 15, to: currentDate)!
+                let timeline = Timeline(entries: [cryptoData], policy: .after(nextUpdate))
+                completion(timeline)
+            }
+        }
+    }
+    
+    func fetchData() async throws -> Crypto {
+        let session = URLSession(configuration: .default)
+        let response = try await session.data(from: URL(string: APIURL)!)
+        let cryptoData = try JSONDecoder().decode([Crypto].self, from: response.0)
+        
+        if let crypto = cryptoData.first {
+            return crypto
+        }
+        return .init()
+    }
+}
+
+fileprivate let APIURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d"
+
+struct Crypto: TimelineEntry, Codable {
+    var date: Date = .init()
+    var priceChange: Double = 0.0
+    var currentPrice: Double = 0.0
+    var last7Days: SparklineData = .init()
+    
+    enum CodingKeys: String, CodingKey {
+        case priceChange = "price_change_percentage_7d_in_currency"
+        case currentPrice = "current_price"
+        case last7Days = "sparkline_in_7d"
+    }
+}
+
+struct SparklineData: Codable {
+    var price: [Double] = []
+    
+    enum CodingKeys: String, CodingKey {
+        case price = "price"
+    }
+}
+
+struct CryptoLiveWidgetEntryView : View {
+    var crypto: Provider.Entry
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color("WidgetBackground"))
+            
+            VStack {
+                HStack {
+                    Image("bitcoin")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 40)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Bitcoin")
+                            .foregroundColor(.white)
+                        
+                        Text("BTC")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text(crypto.currentPrice.toCurrency())
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
+                
+                HStack(spacing: 15) {
+                    VStack(spacing: 8) {
+                        Text("This Week")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        Text(crypto.priceChange.toString(floatingPoint: 1) + "%")
+                            .fontWeight(.semibold)
+                            .foregroundColor(crypto.priceChange < 0 ? .red:Color("Green"))
+                    }
+                    
+                    Chart {
+                        ForEach(crypto.last7Days.price.indices, id: \.self) { index in
+                            LineMark(x: .value("Hour", index), y:  .value("Price", crypto.last7Days.price[index]))
+                        }
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+@main
+struct CryptoLiveWidget: Widget {
+    let kind: String = "CryptoLiveWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            CryptoLiveWidgetEntryView(crypto: entry)
+        }
+        .supportedFamilies([.systemMedium])
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct CryptoLiveWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        CryptoLiveWidgetEntryView(crypto: Crypto(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+    }
+}
+
+extension Double {
+    func toCurrency() -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        
+        return formatter.string(from: .init(value: self)) ?? "$0.00"
+    }
+    
+    func toString(floatingPoint: Int) -> String {
+        let string = String(format: "%.\(floatingPoint)f", self)
+        return string
+    }
+}
